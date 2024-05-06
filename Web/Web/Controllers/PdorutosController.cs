16a5e2851f8e639bc29d4af3b410b16d32c9f2a4
mariele <101908283+masriele@users.noreply.github.com>
2024-05-06 17:05:10 -0300

    swagger

---
+++ Web/Web/Controllers/PdorutosController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+using System.Collections.Generic;
+
+
+namespace ProdutosAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutosController : ControllerBase
+    {
+        private static List<Produtos> _produtos = new List<Produtos>();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_produtos);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Produtos produto)
+        {
+            _produtos.Add(produto);
+            return CreatedAtAction(nameof(Get), new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Produtos produto)
+        {
+            var index = _produtos.FindIndex(p => p.Id == id);
+            if (index == -1)
+                return NotFound();
+
+            _produtos[index] = produto;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var produto = _produtos.Find(p => p.Id == id);
+            if (produto == null)
+                return NotFound();
+
+            _produtos.Remove(produto);
+            return NoContent();
+        }
+    }
+}
---
+++ Web/Web/Produtos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Web.Models;
+
+public class Produtos
+{
+    public int Id { get; set; }
+    public string Name { get; set; } 
+    public string Preco {  get; set; }
+}
